24c3377ddbac8d370feb8b9dc220b28b0bfed136
Ted Cowan <tcowan@my321.net>
2021-01-08 13:54:10 -0700

    Remove all occurrences of puts to log in steps.rb

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,30 +1,30 @@
 Given /^OUTPUT is printed/ do
     stdoutOutput = all_commands.map { |c| c.stdout }.join("\n").strip
     if stdoutOutput != ""
-        puts "STDOUT>>>"
-        puts stdoutOutput
-        puts "<<<STDOUT"
+        log "STDOUT>>>"
+        log stdoutOutput
+        log "<<<STDOUT"
     else
-        puts "STDOUT is EMPTY"
+        log "STDOUT is EMPTY"
     end
     stderrOutput = all_commands.map { |c| c.stderr }.join("\n").strip
     if stderrOutput != ""
-        puts "STDERR>>>"
-        puts stderrOutput
-        puts "<<<STDERR"
+        log "STDERR>>>"
+        log stderrOutput
+        log "<<<STDERR"
     else
-        puts "STDERR is EMPTY"
+        log "STDERR is EMPTY"
     end
 end
 
 Given /^a folder of assorted files in (.*)/ do |folder|
 	@testFiles = TestFiles.new(folder)
-	#puts "Top folder: #{@testFiles.topFolder}"
+	#log "Top folder: #{@testFiles.topFolder}"
 end
 
 Given /^a huge folder of assorted files in (.*)/ do |folder|
 	@testFiles = TestFiles.new(folder, huge=true)
-	#puts "Top folder: #{@testFiles.topFolder}"
+	#log "Top folder: #{@testFiles.topFolder}"
 end
 
 Given /^header contains SearchReport HOSTNAME PATH$/ do
@@ -40,7 +40,7 @@ end
 Given /^header ends with the current date$/ do
 	date = `date`.chomp
 	hostname = `hostname`.chomp
-	#puts "Look TED, all_output: #{all_output}"
+	#log "Look TED, all_output: #{all_output}"
 	# dates look like this: Mon May 11 14:12:24 MDT 2015
 	temp_output = all_commands.map { |c| c.output }.join("\n")
 	if !temp_output.match(/SearchReport\s+#{hostname}\s+#{@testFiles.topFolder}\s+([MTWFS][a-z][a-z])\s+([JFMASOND][a-z][a-z])\s+([ \d]\d)\s+(\d\d)\:(\d\d)\:(\d\d)\s+([A-Z]{3})\s+(\d{4})/)
@@ -54,8 +54,8 @@ Given /^header ends with the current date$/ do
 	second = $6.to_i
 	hisDate = Time.new(yyyy,mon,day,hour,minute,second)
 	now = Time.now
-	#puts "His date: #{hisDate}"
-	#puts "My date: #{now}"
+	#log "His date: #{hisDate}"
+	#log "My date: #{now}"
 	#if (now - hisDate).abs > 60*60*24
 	if (now - hisDate).abs > 60 
 		raise("#{all_output} does not contain a current date as produced by the Linux date command")	
@@ -99,15 +99,15 @@ Given /^total file size is correct$/ do
 end
 
 Given /^the output should contain PWD$/ do 
-	#puts ENV['PWD']
-	#puts ENV['HOME']
-	#puts @dirs.join("/")
+	#log ENV['PWD']
+	#log ENV['HOME']
+	#log @dirs.join("/")
 	#step "the output should match /" + Regexp.escape(ENV['PWD']) + "/"
 	step "the output should contain \"" +  ENV['PWD'] + "/" + @dirs.join("/") + "\""
 end
 
 Given /^(.*) points are awarded/ do |points|
-	#puts "#{points} points are now awarded!!!"
+	#log "#{points} points are now awarded!!!"
 	$total_points += points.to_i
 end
 
@@ -119,7 +119,7 @@ Given /^timeout is increased by (.*) seconds$/ do |seconds|
 	if @aruba_timeout_seconds  
 		@aruba_timeout_seconds += seconds.to_i
 	else
-		puts "aruba_timeout_seconds is NIL!"
+		log "aruba_timeout_seconds is NIL!"
 	end
 end
 
@@ -127,7 +127,7 @@ Given /^timeout is decreased by (.*) seconds$/ do |seconds|
 	if @aruba_timeout_seconds
 		@aruba_timeout_seconds -= seconds.to_i
 	else
-		puts "aruba_timeout_seconds is NIL!"
+		log "aruba_timeout_seconds is NIL!"
 	end
 end
 
