3bee786c552288ccf11bb7a110806ebf19d9e89a
Ted Cowan <tcowan@my321.net>
2021-01-07 19:46:33 -0700

    Undid puts to log in grading.rb.

--- features/support/grading.rb
+++ features/support/grading.rb
@@ -1,33 +1,33 @@
 $total_points = 0
 
 Before do
-	# log Dir.pwd
+	# puts Dir.pwd
 	ENV['LIBC_FATAL_STDERR_'] = "1"
 
 	if File.file?('/tmp/jenkins')
-		#log "Found leftover file /tmp/jenkins...deleting"
+		#puts "Found leftover file /tmp/jenkins...deleting"
 		File.delete('/tmp/jenkins')
 	end
 
 	if File.directory?('/tmp/jenkins')
-		#log "Found leftover folder /tmp/jenkins...deleting"
+		#puts "Found leftover folder /tmp/jenkins...deleting"
 		FileUtils.rm_rf('/tmp/jenkins')
 	end
 end
 
 After do
 	if File.file?('/tmp/jenkins')
-		log "WARNING: Found leftover file /tmp/jenkins...deleting"
+		puts "WARNING: Found leftover file /tmp/jenkins...deleting"
 		File.delete('/tmp/jenkins')
 	end
 
 	if File.directory?('/tmp/jenkins')
-		log "NOTICE: Found leftover folder /tmp/jenkins...deleting"
+		puts "NOTICE: Found leftover folder /tmp/jenkins...deleting"
 		FileUtils.rm_rf('/tmp/jenkins')
 	end
 
 end
 
 at_exit do
-	log "A total of #{$total_points} points have been awarded."
+	puts "A total of #{$total_points} points have been awarded."
 end
